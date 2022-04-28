e18b1898e21b561f6e974ac2a96bde7597cff109
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-04-28 18:33:02 +0300

    Implement bubble sorting

--- src/strategy/sortingFunctions.ts
+++ src/strategy/sortingFunctions.ts
@@ -1,5 +1,23 @@
-function bubbleSort() {
-
+function bubbleSort(nums: number[] | []) {
+  if (nums.length === 0) {
+    return [];
+  }
+  let exchanges = true;
+  let iterationsCount = 0;
+  const res = [...nums];
+  while (exchanges && iterationsCount < nums.length) {
+    exchanges = false;
+    for (let j = 0; j < res.length - 1; j += 1) {
+      if (res[j] > res[j + 1]) {
+        const x = res[j];
+        res[j] = res[j + 1];
+        res[j + 1] = x;
+        exchanges = true;
+      }
+    }
+    iterationsCount += 1;
+  }
+  return res;
 }
 
 export { bubbleSort };
