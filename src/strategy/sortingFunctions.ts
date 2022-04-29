5640c29bec5ec0e9e4557ace1667bb101c7d201e
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-04-29 11:58:33 +0300

    Implement merge sorting

--- src/strategy/sortingFunctions.ts
+++ src/strategy/sortingFunctions.ts
@@ -1,4 +1,4 @@
-function bubbleSort(nums: number[]): number[] {
+function bubbleSort(nums: number[]):number[] {
   if (nums.length === 0) {
     return [];
   }
@@ -21,7 +21,37 @@ function bubbleSort(nums: number[]): number[] {
 }
 
 function mergeSort(nums: number[]):number[] {
-  
+  if (nums.length < 2) {
+    return nums;
+  }
+  const midPosition = Math.round(nums.length / 2);
+  const leftHalf = mergeSort(nums.slice(0, midPosition));
+  const rightHalf = mergeSort(nums.slice(midPosition));
+  let leftHalfIndex = 0;
+  let rightHalfIndex = 0;
+  const res: number[] = Array(nums.length);
+  let resIndex = 0;
+  while (leftHalfIndex < leftHalf.length && rightHalfIndex < rightHalf.length) {
+    if (leftHalf[leftHalfIndex] < rightHalf[rightHalfIndex]) {
+      res[resIndex] = leftHalf[leftHalfIndex];
+      leftHalfIndex += 1;
+    } else {
+      res[resIndex] = rightHalf[rightHalfIndex];
+      rightHalfIndex += 1;
+    }
+    resIndex += 1;
+  }
+  while (leftHalfIndex < leftHalf.length) {
+    res[resIndex] = leftHalf[leftHalfIndex];
+    leftHalfIndex += 1;
+    resIndex += 1;
+  }
+  while (rightHalfIndex < rightHalf.length) {
+    res[resIndex] = rightHalf[rightHalfIndex];
+    rightHalfIndex += 1;
+    resIndex += 1;
+  }
+  return res;
 }
 
 export { bubbleSort, mergeSort };
