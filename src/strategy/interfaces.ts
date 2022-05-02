2c7fed7bf288683a0f100ae771cbe959a22fe699
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-02 11:57:35 +0300

    Delete implementation details from ISort int

--- src/strategy/interfaces.ts
+++ src/strategy/interfaces.ts
@@ -1,9 +1,4 @@
-import ImmutableSortFunc from './commonTypes';
-
 interface ISortAlg {
-  readonly description: string;
-  readonly sortAlg: ImmutableSortFunc;
-  new (sortAlg: ImmutableSortFunc, desciprtion: string): ISortAlg;
   sort(nums: number[]): number[];
 }
 
