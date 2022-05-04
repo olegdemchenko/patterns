8c33b11fe097ce7a9de4d55e3c5438f1b3e769c2
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-04 18:47:31 +0300

    Create interfaces for the Observer implementation

---
+++ src/observer/interfaces.ts
@@ -0,0 +1,17 @@
+interface INewsItem {
+  title: string;
+  link: string;
+  date: Date;
+}
+
+interface IObserver {
+  update: (data: INewsItem) => void;
+}
+
+interface IEventEmitter {
+  registerObserver: (observer: IObserver) => void;
+  unregisterObserver: (observer: IObserver) => void;
+  notify: (data: INewsItem) => void;
+}
+
+export { IObserver, IEventEmitter };
