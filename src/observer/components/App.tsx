7fe1b8d84c5d00026cfcef357b9a6c3c3b88e2d3
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-06 10:28:04 +0300

    Add Observer app base components

--- src/observer/components/App.tsx
+++ src/observer/components/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { IEventEmitter } from '../interfaces';
+import Container from './Container';
+import Header from './Header';
+import Feeds from './Feeds';
 
 type AppProps = {
   rssEmitter:IEventEmitter;
 };
 
 function App({ rssEmitter }: AppProps) {
-  return <div>Hello world!</div>;
+  return (
+    <Container>
+      <Header />
+      <Feeds />
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/observer/components/Container.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type ContainerProps = {
+  children: JSX.Element[]
+};
+
+function Container(props: ContainerProps) {
+  return <div>Header</div>;
+}
+
+export default Container;
