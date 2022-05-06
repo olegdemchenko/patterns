1949b65d9faef763a958f70539c1b4fe9479d014
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-06 11:05:27 +0300

    Apply bootstrap styles for the Observer app

--- src/observer/index.tsx
+++ src/observer/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import RSSFeedEmitter from 'rss-feed-emitter';
 import App from './components/App';
 import RSSEventMediator from './RSSEventMediator';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <App rssEmitter={new RSSEventMediator(new RSSFeedEmitter())} />,
