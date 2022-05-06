03ffff9ef6b80acbbdb9a3a0cb66e0b7f0690871
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-06 11:28:02 +0300

    Create Header comp base structure

--- src/observer/components/Header.tsx
+++ src/observer/components/Header.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="pt-2 pb-3 bg-dark rounded text-center text-light">
+      <h1 className="mb-0">RSS Reader</h1>
+      <p className="mb-0">Start reading RSS today! It is easy, it is nicely.</p>
+    </div>
+  );
 }
 
 export default Header;
