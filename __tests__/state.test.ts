c578db45a3b53a1cb229a388a8ab0b2c03001599
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-09 18:09:35 +0300

    Add test to check clock state behavior

--- __tests__/state.test.ts
+++ __tests__/state.test.ts
@@ -28,3 +28,16 @@ test('check time changing in clock state', () => {
   expect(clock.minutes()).toBe('01');
   expect(clock.hours()).toBe('01');
 });
+
+test('check clock state behavior', () => {
+  expect(clock.getCurrentMode()).toBe('clock');
+  clock.clickH();
+  clock.clickH();
+  clock.clickM();
+  clock.clickM();
+  expect(clock.hours()).toBe('02');
+  expect(clock.minutes()).toBe('02');
+  jest.advanceTimersByTime(61 * 60 * 1000);
+  expect(clock.hours()).toBe('03');
+  expect(clock.minutes()).toBe('03');
+});
