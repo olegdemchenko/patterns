f9f3bbd2a4c60951b08d923078d6ae4388497a58
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-09 17:58:46 +0300

    Factorize time in tests

--- __tests__/state.test.ts
+++ __tests__/state.test.ts
@@ -18,10 +18,10 @@ test('check time changing in clock state', () => {
   expect(clock.getCurrentMode()).toBe('clock');
   expect(clock.minutes()).toBe('00');
   expect(clock.hours()).toBe('00');
-  jest.advanceTimersByTime(60000);
+  jest.advanceTimersByTime(60 * 1000);
   expect(clock.minutes()).toBe('01');
   expect(clock.hours()).toBe('00');
-  jest.advanceTimersByTime(3600000);
+  jest.advanceTimersByTime(60 * 60 * 1000);
   expect(clock.minutes()).toBe('01');
   expect(clock.hours()).toBe('01');
   jest.advanceTimersByTime(24 * 60 * 60 * 1000);
