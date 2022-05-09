6a7ce22150a2df969e5a72f65ade7b38e259ccd6
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-05-09 17:50:52 +0300

    Fix errors in test related to time changing

--- __tests__/state.test.ts
+++ __tests__/state.test.ts
@@ -6,7 +6,7 @@ beforeAll(() => {
   jest.useFakeTimers();
 });
 
-beforeAll(() => {
+afterAll(() => {
   jest.useRealTimers();
 });
 
@@ -18,7 +18,7 @@ test('check time changing in clock state', () => {
   expect(clock.getCurrentMode()).toBe('clock');
   expect(clock.minutes()).toBe('00');
   expect(clock.hours()).toBe('00');
-  jest.advanceTimersByTime(6000);
+  jest.advanceTimersByTime(60000);
   expect(clock.minutes()).toBe('01');
   expect(clock.hours()).toBe('00');
   jest.advanceTimersByTime(3600000);
